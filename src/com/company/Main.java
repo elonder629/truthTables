0972e9d264574359b19ba9fd252a511f2ec56e37
Eric Londer <ericlonder730@gmail.com>
2017-12-15 02:59:13 -0500

    simplified and cooler than Dan's giant input.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,6 @@
 package com.company;
 
-import com.sun.org.apache.xpath.internal.SourceTree;
-
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 import javax.script.ScriptEngineManager;
@@ -13,69 +10,60 @@ import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) throws ScriptException, IOException{
+    public static void main(String[] args) throws ScriptException, IOException {
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter a boolean expression int the format of 'A && B || C'");
         String expressionScan = input.nextLine().toLowerCase();
         FileWriter fw = new FileWriter("theTruthWillSetYouFree.out");
         PrintWriter output = new PrintWriter(fw);
-
         String[] booleanVariableNames = expressionScan.split("\\s+");
         int varCounter = 0;
-        for (int x = 0; x < booleanVariableNames.length; x++){
-            if (!booleanVariableNames[x].contains("&&")
-                    && !booleanVariableNames[x].contains("||")
-                    && !booleanVariableNames[x].contains("!=")) {
+        for (String booleanVariableName1 : booleanVariableNames) {
+            if (!booleanVariableName1.contains("&&")
+                    && !booleanVariableName1.contains("||")
+                    && !booleanVariableName1.contains("!="))
                 varCounter++;
-            }
         }
 
         ScriptEngineManager mgr = new ScriptEngineManager();
         ScriptEngine engine = mgr.getEngineByName("JavaScript");
         output.println("Expression: " + expressionScan);
-        for (int g = 0; g < booleanVariableNames.length; g++){
-            if (!booleanVariableNames[g].contains("&&")
-                    && !booleanVariableNames[g].contains("||")
-                    && !booleanVariableNames[g].contains("!=")) {
-                output.print("\t" + booleanVariableNames[g]);
-            }
+        for (String booleanVariableName : booleanVariableNames) {
+            if (!booleanVariableName.contains("&&")
+                    && !booleanVariableName.contains("||")
+                    && !booleanVariableName.contains("!="))
+                output.print("\t" + booleanVariableName);
         }
         output.print("\t\t" + expressionScan + "\n");
 
-        int n = varCounter;
-        for (int i = 0; i < Math.pow(2, n); i++) {
-            String bin = Integer.toBinaryString(i);
-            while (bin.length() < n)
-                bin = "0" + bin;
-            char[] chars = bin.toCharArray();
-            boolean[] boolArray = new boolean[n];
+        for (int i = 0; i < Math.pow(2, varCounter); i++) {
+            StringBuilder bin = new StringBuilder(Integer.toBinaryString(i));
+            while (bin.length() < varCounter)
+                bin.insert(0, "0");
+            char[] chars = bin.toString().toCharArray();
+            boolean[] boolArray = new boolean[varCounter];
             for (int j = 0; j < chars.length; j++) {
                 boolArray[j] = chars[j] == '0';
             }
-            output.println(Arrays.toString(boolArray) + "\t\t" + engine.eval(booleanExpressionBuilder(boolArray, booleanVariableNames)));
-
+            output.println(Arrays.toString(boolArray) +
+                    "\t\t" + engine.eval(booleanExpressionBuilder(boolArray, booleanVariableNames)));
         }
         fw.close();
         output.close();
-
     }
 
-    public static String booleanExpressionBuilder(boolean[] values, String[] expression){
-        String returnString = "";
+    private static String booleanExpressionBuilder(boolean[] values, String[] expression) {
+        StringBuilder returnString = new StringBuilder();
         int valuePlace = 0;
-        for (int x = 0; x < expression.length; x++){
-            if (!expression[x].contains("&&")
-                    && !expression[x].contains("||")
-                    && !expression[x].contains("!=")) {
-                returnString += values[valuePlace];
+        for (String anExpression : expression) {
+            if (!anExpression.contains("&&")
+                    && !anExpression.contains("||")
+                    && !anExpression.contains("!=")) {
+                returnString.append(values[valuePlace]);
                 valuePlace++;
-            }
-            else {
-                returnString += " " +expression[x] + " ";
-            }
-
+            } else
+                returnString.append(" ").append(anExpression).append(" ");
         }
-        System.out.println(returnString);
-        return returnString;
+        return returnString.toString();
     }
 }
